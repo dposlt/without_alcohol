1ebaa4a82a255a8c4b949162a43e00b0fe4b4934
David Poslt <david.poslt@gmail.com>
2020-02-05 18:27:30 +0100

    add argument -h

--- without_alcohol.py
+++ without_alcohol.py
@@ -16,6 +16,7 @@ init()
 def getResult():
     y = 'OK'
     n = 'KO'
+    help = 'add argument "-y" for day without alcoho \n add argument "-n" for day with alcohol'
 
     countArgs = len(sys.argv)
 
@@ -25,13 +26,17 @@ def getResult():
 
     YesOrNo = sys.argv[1]
 
-    if len(YesOrNo) != 1:
+    if len(YesOrNo) != 2:
         print('Incorrect count of arguments')
         sys.exit()
+        
+    if YesOrNo.lower() == '-h':
+        return help
+        sys.exit()
 
-    if YesOrNo.lower() == 'y':
+    if YesOrNo.lower() == '-y':
         return y
-    elif YesOrNo.lower() == 'n':
+    elif YesOrNo.lower() == '-n':
         return n
     else:
         print('Argument must be "y" or "n" ')
